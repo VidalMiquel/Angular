71a933d1b3d74946b3ec01635ff1401ad29fc487
Miquel Vidal <miquelvidalcortes@gmail.com>
2024-09-30 20:43:43 +0200

    add photo and header

--- own-project/my-cv/src/app/my-cv/header/pages/header-page.component.ts
+++ own-project/my-cv/src/app/my-cv/header/pages/header-page.component.ts
@@ -1,18 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { MyCvService } from '../services/my-cv.service';
+import { navItem } from '../interfaces/nav-item.interface';
 
 @Component({
   selector: 'my-cv-header-page',
-  templateUrl: './header-page.component.html'
+  templateUrl: './header-page.component.html',
+  styleUrl: './header-page.component.css'
 })
 
 //FATHER
+
 export class HeaderPageComponent {
 
-/*
+
   constructor(public myCvService:MyCvService){}
 
   get navItems(): navItem[]{
-    return [...this.myCvService.navItems];
+    return this.myCvService.navItems;
   }
-*/
+
 }
