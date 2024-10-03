cf387df49907333e5c226772f3bb41272ba28020
Miquel Vidal <mvidalc@riu.com>
2024-10-03 16:50:22 +0200

    started PipeProject; defining routing and PrimeNG configuration

---
+++ 05-pipesApp/src/app/products/pages/basics-page/basics-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-basics-page',
+  templateUrl: './basics-page.component.html',
+  styleUrl: './basics-page.component.css'
+})
+export class BasicsPageComponent {
+  public nameLower:string = "miquel";
+  public nameUpper:string = "MIQUEL";
+  public fullName:string = "MiQuEl";
+}
