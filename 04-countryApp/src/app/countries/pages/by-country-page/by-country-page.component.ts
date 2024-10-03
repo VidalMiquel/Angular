ca33531c6abf7bb1cec49f5b66cbf7065a7abbbf
Miquel Vidal <mvidalc@riu.com>
2024-10-03 12:00:42 +0200

    localStorage done for each funcionality and project finished

--- 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
+++ 04-countryApp/src/app/countries/pages/by-country-page/by-country-page.component.ts
@@ -10,12 +10,20 @@ import { CountriesService } from '../../services/countries.service';
 export class ByCountryPageComponent {
 
   public countries: Country[] = []
+  public initialValue: string = '';
+  public isLoading: boolean = false
   constructor(private countriesSErvice:CountriesService){}
 
+  ngOnInit(): void {
+    this.countries = this.countriesSErvice.cacheStore.byCountries.countries;
+    this.initialValue = this.countriesSErvice.cacheStore.byCountries.term;
+  }
   searchByCountry(term:string):void{
+    this.isLoading = true
     //we have to sbuscribe to the Observable(definded at Service)
     this.countriesSErvice.searchCountry(term).subscribe(countires =>{
       this.countries = countires
+      this.isLoading = false;
     });
   }
 }
