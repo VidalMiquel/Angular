0f9aa9290c44cfa42f520390fa259d526e0b11da
Miquel Vidal <mvidalc@riu.com>
2024-10-10 16:16:17 +0200

    finsished  06-heroesApp project by adding guards authentication

--- 06-heroesApp/src/app/app-routing.module.ts
+++ 06-heroesApp/src/app/app-routing.module.ts
@@ -2,16 +2,26 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HeoresModule } from './heores/heores.module';
 import { Error404PageComponent } from './shared/pages/error404-page/error404-page.component';
+import { AuthGuard } from './auth/guards/auth.guard';
+import { PublicGuard } from './auth/guards/public.guard';
 
 //https://localhost:4200
 const routes: Routes = [
 {
   path:'auth',
   loadChildren: () => import ('./auth/auth.module').then(m=> m.AuthModule),
+  canActivate: [PublicGuard],
+  canMatch:[
+    PublicGuard
+  ]
 },
 {
   path:'heroes',
   loadChildren: () => import ('./heores/heores.module').then(m=> m.HeoresModule),
+  canActivate: [AuthGuard],
+  canMatch:[
+    AuthGuard
+  ]
 },
 {
   path:'404',
